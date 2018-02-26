4898e9cb0f5847e97dbe9c5b0106dcd447ab66fc
SteyaertJamainFrigge <frigge.steyaert.jamain@student.howest.be>
2018-02-26 11:44:57 +0100

    solutionnaam verandert

---
+++ Shopping List/Models/ShoppingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping_List.Models
+{
+
+    public class ShoppingModel
+    {
+        public int Id { get; set; }
+
+        public string Naam { get; set; }
+
+    }
+}
